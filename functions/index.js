2e808bfc9dc536f47a82b726b2ae0344d9ba8ff8
Mauro Minenna <mauro@minenna.it>
2017-09-30 21:00:03 +0000

    Ho un rudimento della gestione di magazzino... Posso lavorare per agganciare rudimentalmente le vendite.

--- functions/index.js
+++ functions/index.js
@@ -6,43 +6,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-/*
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  admin.database().ref('/messages').push({original: original}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
-});
-*/
-
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-/*
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-    .onWrite(event => {
-      // Grab the current value of what was written to the Realtime Database.
-      const original = event.data.val();
-      console.log('Uppercasing', event.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return event.data.ref.parent.child('uppercase').set(uppercase);
-    });
-*/    
     
-
-    
-exports.calcolaTotaleBolla = functions.database.ref('{catena}/{negozio}/bolle/{idBolla}')
-    .onWrite(event => {
-    	if (event.data.child('righe').changed()) //Non eseguo se sono cambiati i totali...
-    		{
-        	const righe = event.data.child('righe').val();
+exports.calcolaTotaleBolla = functions.database.ref('{catena}/{negozio}/bolle/{idBolla}/righe')
+    .onWrite(event => 
+            {
+        	const righe = event.data.val();
         	var totalePezzi = 0.0;
 			var totaleGratis = 0.0;
 			var totaleImporto = 0.0;
@@ -53,14 +21,75 @@ exports.calcolaTotaleBolla = functions.database.ref('{catena}/{negozio}/bolle/{i
 			    totaleImporto =  parseFloat(righe[propt].prezzoTotale) + parseFloat(totaleImporto);
 				}
 			const totali = {'pezzi' : totalePezzi, 'gratis' : totaleGratis, 'prezzoTotale' : totaleImporto.toFixed(2)}; 
-			console.log(totali);
-			return event.data.ref.child('totali').set(totali);	
+			console.info("Aggiornati totali");
+			return event.data.ref.parent.child('totali').set(totali);	
 			
     		}
-    	else return event.data.ref; //Nessuna modifica se sono cambiati i totali...
-    }); 
-    
+           ); 
+//Il registro è organizzato per catena -> Negozio -> EAN -> keyDocumento che origina il valore...
+//Nel caso delle bolle... devo valorizzare la quantità come somma di copie e copie gratis. 
 
+exports.aggiornaRegistroDaBolla = functions.database.ref('{catena}/{negozio}/bolle/{idBolla}/righe/{keyRiga}')
+    .onWrite(event => 
+            {
+            const key = event.params.keyRiga;	
+            //Caso riga cancellata....recupero dal passato valore EAN e chiave... e cancello la entry corrispondente 
+         
+            if (!event.data.exists()) 
+                  {
+                  const ean = event.data.previous.val().ean;
+                  return event.data.ref.parent.parent.parent.parent.child('registro/'+ean+'/'+key).remove();	
+                  }
+            else 
+            	  {
+            	  const ean = event.data.val().ean;	
+            	  const newVal = Object.assign(event.data.val(), {tipo: 'bolla', id: event.params.idBolla});
+            	  return event.data.ref.parent.parent.parent.parent.child('registro/'+ean+'/'+key).set(newVal);
+            	  }
+               
+            //Caso riga inserita o modificata... la sostituisco integralmente. 
+            
+            }
+           ); 
+
+exports.aggiornaMagazzino = functions.database.ref('{catena}/{negozio}/registro/{ean}')
+    .onWrite(event => 
+            {
+            const ean = event.params.ean;	
+            //Caso riga cancellata....cancello semplicemente EAN ... e cancello la entry corrispondente 
+         
+            if (!event.data.exists()) 
+                  {
+                  return event.data.ref.parent.parent.child('magazzino/'+ean).remove();	
+                  }
+            else 
+            	  {
+            	  var totalePezzi = 0;
+        		  var titolo;
+                  var autore;
+          	      
+            	  righe = event.data.val();	
+            	  for(var propt in righe)
+		  				{
+		  				 if (righe[propt].tipo == "bolla")
+		  					{
+			    			totalePezzi = parseInt(righe[propt].pezzi) + parseInt(righe[propt].gratis)+ totalePezzi;
+			    			titolo =  righe[propt].titolo;
+			    			autore =   righe[propt].autore; 
+			    			//parseFloat(righe[propt].prezzoTotale) + parseFloat(totaleImporto);
+							}
+		  				}	
+			      const totali = {'pezzi' : totalePezzi, 'titolo' : titolo, 'autore' : autore}; 
+            	  return event.data.ref.parent.parent.child('magazzino/'+ean).set(totali);
+            	  }
+               
+            //Caso riga inserita o modificata... la sostituisco integralmente. 
+            
+            }
+           ); 
+    
+//Questa resta una traccia per utilizzare adminSDK e scrivere con una transazione... come dire TUTTO IL CUCUZZARO...
+/*
 exports.calcolaTotaleBollaCorreggere = functions.database.ref('{catena}/{negozio}/bolle/{idBolla}/righe')
     .onWrite(event => {
     	const righe = event.data.val();
@@ -92,4 +121,6 @@ exports.calcolaTotaleBollaCorreggere = functions.database.ref('{catena}/{negozio
 		  
     	return event.data.ref; //Nessuna modifica
     }); 
+    
+    */
    
